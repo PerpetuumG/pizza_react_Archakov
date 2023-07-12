0dae021356027d720c531412df2ebf909d9b467a
PerpetuumG <perpetuumg@gmail.com>
2023-07-12 16:28:14 +0300

    get data from server

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
@@ -5,9 +8,25 @@ import { PizzaBlock } from './components/PizzaBlock';
 
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
+// импорт данных из файла src/assets/pizzas.json
+// import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const itemsResponse = await axios.get('https://64aea1a3c85640541d4d6f3a.mockapi.io/items');
+        setItems(itemsResponse.data);
+      } catch (e) {
+        alert('Ошибка при запросе данных :(');
+        console.error(e);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,7 +38,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map(obj => (
+            {items.map(obj => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
