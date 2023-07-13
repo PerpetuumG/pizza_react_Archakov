29fe0acde662a9f06a87dc7a1c7fbd2fc26d3bc4
PerpetuumG <perpetuumg@gmail.com>
2023-07-13 14:10:13 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
 import { Header } from './components/Header';
-import { Categories } from './components/Categories';
-import { Sort } from './components/Sort';
-import { PizzaBlock } from './components/PizzaBlock/PizzaBlock';
-import { Skeleton } from './components/PizzaBlock/Skeleton';
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { NotFound } from './pages/NotFound';
 
 import './scss/app.scss';
 
@@ -13,38 +11,16 @@ import './scss/app.scss';
 // import pizzas from './assets/pizzas.json';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const itemsResponse = await axios.get('https://64aea1a3c85640541d4d6f3a.mockapi.io/items');
-        setItems(itemsResponse.data);
-      } catch (e) {
-        alert('Ошибка при запросе данных :(');
-        console.error(e);
-      }
-      setIsLoading(false);
-    }
-    fetchData();
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/cart'} element={<Cart />} />
+            <Route path={'*'} element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { Categories } from '../components/Categories';
+import { Sort } from '../components/Sort';
+import { Skeleton } from '../components/PizzaBlock/Skeleton';
+import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
+
+export const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const itemsResponse = await axios.get('https://64aea1a3c85640541d4d6f3a.mockapi.io/items');
+        setItems(itemsResponse.data);
+      } catch (e) {
+        alert('Ошибка при запросе данных :(');
+        console.error(e);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
