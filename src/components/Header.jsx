a9e615a665ad6694443366842f5f0964c17acdec
PerpetuumG <perpetuumg@gmail.com>
2023-07-16 13:12:20 +0300

    add search by list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import logoSvg from '../assets/img/pizza-logo.svg';
 import { Link } from 'react-router-dom';
+import { Search } from './Search/Search';
 
-export const Header = () => {
+export const Header = ({ searchValue, setSearchValue }) => {
   return (
     <div className="header">
       <div className="container">
+        {/* Logo */}
         <Link to="/">
           <div className="header__logo">
             <img width="38" src={logoSvg} alt="Pizza logo" />
@@ -14,6 +16,11 @@ export const Header = () => {
             </div>
           </div>
         </Link>
+
+        {/* Search */}
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+
+        {/* button Cart */}
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 €</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { Sort } from '../components/Sort';
 import { Skeleton } from '../components/PizzaBlock/Skeleton';
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
 
-export const Home = () => {
+export const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
@@ -21,11 +21,12 @@ export const Home = () => {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     async function fetchData() {
       try {
         const itemsResponse = await axios.get(
-          `https://64aea1a3c85640541d4d6f3a.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+          `https://64aea1a3c85640541d4d6f3a.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`,
         );
         setItems(itemsResponse.data);
       } catch (e) {
@@ -38,7 +39,10 @@ export const Home = () => {
 
     // переместиться в начало страницы
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />);
 
   return (
     <div className={'container'}>
@@ -47,11 +51,7 @@ export const Home = () => {
         <Sort value={sortType} onChangeSort={i => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
     </div>
   );
 };
